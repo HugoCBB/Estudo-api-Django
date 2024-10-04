d7e2f307f77f4398b8a511d821acc044dfb197aa
Hugo <hugocarlosb.071@gmail.com>
2024-10-03 21:43:00 -0300

    Apps instalados

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'myapi.apps.MyapiConfig'
+    'myapi.apps.MyapiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
